190f2f9a5e7dd87706ba21eb5f0c06b84f605e43
RianDelou <riandelou63@hotmail.com>
2024-02-28 00:07:30 -0300

    finalizado

--- script.js
+++ script.js
@@ -6,15 +6,20 @@ const img = document.querySelector(".app__image");
 const title = document.querySelector(".app__title");
 const buttonList = document.querySelectorAll(".app__card-button");
 const musicButton = document.getElementById("alternar-musica");
+const buttonTimer = document.getElementById("start-pause");
+const buttonTimerText = document.querySelector("#start-pause span");
+const ButtonTimerImg = document.querySelector(".app__card-primary-butto-icon");
+const timerPlace = document.getElementById("timer");
+
 const musicaPadrao = new Audio("sons/luna-rise-part-one.mp3");
 musicaPadrao.loop = true;
-const buttonStartOrPauseTimer = document.getElementById("start-pause");
 const musicList = [new Audio("sons/play.wav"), new Audio("sons/pause.mp3"), new Audio("sons/beep.mp3")];
 
-let tempo = 5;
+let tempo = 1500;
 let intervaloValue = null;
 
 function eventoBotao(valor) {
+    timerAppear(); //atualiza o tempo
 
     buttonList.forEach(conteudo => {
         conteudo.classList.remove("active");
@@ -55,36 +60,51 @@ function eventoBotao(valor) {
 function startOrPauseTimer() {
 
     if (intervaloValue) { //caso intervaloValue exista, não seja nulo
-        clearInterval(intervaloValue);
-        intervaloValue = null;
-
+        zerar();
         musicList[1].play(); //pause
         return;
     } else {
-        musicList[0].play(); //play
+        buttonTimerText.textContent = "Pausar";
+        ButtonTimerImg.setAttribute("src", "imagens/pause.png");
+        musicList[0].play(); //play START
     }
 
     intervaloValue = setInterval(() => {
         if (tempo <= 0) {
-            clearInterval(intervaloValue);
-            console.log("temporizador finalizado");
+            zerar();
             musicList[2].play(); //beep beep
             return;
         }
-        console.log(tempo);
         tempo -= 1;
+        timerAppear();
     }, 1000);
 }
 
+function zerar() {
+    clearInterval(intervaloValue);
+    intervaloValue = null;
+    buttonTimerText.textContent = "Começar";
+    ButtonTimerImg.setAttribute("src", "imagens/play_arrow.png");
+}
+
+function timerAppear() {
+    const timeData = new Date(tempo * 1000); //o objeto date trabalha apenas com millissegundos
+    const tempoFormatado = timeData.toLocaleTimeString("pt-Br", {minute: "2-digit", second: "2-digit"});
+    timerPlace.textContent = tempoFormatado;
+}
+
 foco.addEventListener("click", () => {
+    tempo = 1500; // 25 m
     eventoBotao("foco");
 });
 
 short.addEventListener("click", () => {
+    tempo = 300; // 5 m
     eventoBotao("descanso-curto");
 });
 
 long.addEventListener("click", () => {
+    tempo = 900; // 15 m
     eventoBotao("descanso-longo");
 });
 
@@ -96,4 +116,6 @@ musicButton.addEventListener("change", () => {
     }
 });
 
-buttonStartOrPauseTimer.addEventListener("click", startOrPauseTimer);
+buttonTimer.addEventListener("click", startOrPauseTimer);
+
+timerAppear();
